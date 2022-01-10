bf9ddde5b175432b85b280b39cfb145741b58729
Nerijus <ner.jatkauskas@gmail.com>
2022-01-10 21:18:16 +0200

    Adeded logic and baisic skeleton of HTML

--- rock-paper-scissors/scripts.js
+++ rock-paper-scissors/scripts.js
@@ -1,6 +1,11 @@
-const rounds = 5;
+const score_to_win = 5;
 let playerScore = 0;
 let computerScore = 0;
+let roundNumber = 0;
+let current_round = document.querySelector(".current_round_result");
+let player_score = document.querySelector(".player_score");
+let computer_score = document.querySelector(".computer_score");
+let round_num = document.querySelector(".round_num");
 
 const capitalize = (s) => {
   if (typeof s !== "string") return "";
@@ -14,39 +19,40 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
-  // console.log(playerSelection + " and " + computerSelection);
-  if (playerSelection) {
-    if (playerSelection.toUpperCase() === computerSelection.toUpperCase()) {
-      return "tie";
-    } else if (
-      (playerSelection.toUpperCase() === "ROCK" &&
-        computerSelection.toUpperCase() === "SCISSORS") ||
-      (playerSelection.toUpperCase() === "SCISSORS" &&
-        computerSelection.toUpperCase() === "PAPER") ||
-      (playerSelection.toUpperCase() === "PAPER" && computerSelection.toUpperCase() === "ROCK")
-    ) {
-      playerScore += 1;
-      return `You Win! ${capitalize(playerSelection)} beats ${capitalize(computerSelection)}`;
+  if (playerSelection.toUpperCase() === computerSelection.toUpperCase()) {
+    current_round.innerText = "Tie";
+    round_num.innerText = roundNumber += 1;
+  } else if (
+    (playerSelection.toUpperCase() === "ROCK" && computerSelection.toUpperCase() === "SCISSORS") ||
+    (playerSelection.toUpperCase() === "SCISSORS" && computerSelection.toUpperCase() === "PAPER") ||
+    (playerSelection.toUpperCase() === "PAPER" && computerSelection.toUpperCase() === "ROCK")
+  ) {
+    current_round.innerText = `You Win! ${capitalize(playerSelection)} beats ${capitalize(computerSelection)}`;
+    player_score.innerText = playerScore += 1;
+    round_num.innerText = roundNumber += 1;
+  } else {
+    current_round.innerText = `You Lose! ${capitalize(computerSelection)} beats ${capitalize(playerSelection)}`;
+    computer_score.innerText = computerScore += 1;
+    round_num.innerText = roundNumber += 1;
+  }
+  checkWinner(playerScore, computerScore);
+}
+
+function checkWinner(player, opponent) {
+  if (player === score_to_win || opponent === score_to_win) {
+    if (player > opponent) {
+      current_round.innerText = `You Win the game! ${player} > ${opponent}`;
     } else {
-      computerScore += 1;
-      return `You Lose! ${capitalize(computerSelection)} beats ${capitalize(playerSelection)}`;
+      current_round.innerText = `You lose the game! ${player} < ${opponent}`;
     }
-  } else {
-    return "Player selection is empty";
   }
 }
 
-function game() {
-  for (let i = 0; i < rounds; i++) {
-    console.log(playRound(prompt("Pick one: Rock, Paper, Scissors"), computerPlay()));
-  }
-  if (playerScore > computerScore) {
-    return "You Win the game! " + playerScore + " > " + computerScore;
-  } else if (playerScore === computerScore) {
-    return "It's a tie! " + playerScore + " = " + computerScore;
-  } else {
-    return "You lose the game! " + playerScore + " < " + computerScore;
-  }
+function addClickEvent(element) {
+  element.addEventListener("click", function (e) {
+    playRound(e.path[0].innerText, computerPlay());
+  });
 }
 
-console.log(game());
+let cards = document.querySelectorAll(".pick");
+cards.forEach(addClickEvent);
